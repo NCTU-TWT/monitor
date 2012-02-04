fdf13a8ba2291d75421728f9c67c99c4a6284fa5
Elliot Lai <ellliotlai@gmail.com>
2012-02-04 15:44:03 +0800

    frontend libs amd compatible update

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,53 +1,50 @@
 (function() {
-  var __hasProp = Object.prototype.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; },
-    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-
+  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+    for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+    function ctor() { this.constructor = child; }
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor;
+    child.__super__ = parent.prototype;
+    return child;
+  }, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   require.config({
     paths: {
       jquery: 'lib/jquery-1.7.1.min',
-      io: 'lib/socket.io-client-amd',
+      io: 'lib/socket.io.min.amd',
       raphael: 'lib/raphael-min',
-      underscore: 'lib/underscore-min',
-      backbone: 'lib/backbone-amd-min',
-      hogan: 'lib/hogan'
+      underscore: 'lib/underscore-min.amd',
+      backbone: 'lib/backbone-min.amd',
+      hogan: 'lib/hogan-1.0.5.min.amd'
     }
   });
-
   require(['jquery', 'io', 'raphael', 'underscore', 'backbone', 'hogan'], function($, io, Raphael, _, Backbone, hogan) {
     var Chart, Mediator, Meter, Oscilloscope, chart, socket;
     Mediator = {};
     _.extend(Mediator, Backbone.Events);
     Chart = {};
-    Chart.Model = (function(_super) {
-
-      __extends(Model, _super);
-
+    Chart.Model = (function() {
+      __extends(Model, Backbone.Model);
       function Model() {
         Model.__super__.constructor.apply(this, arguments);
       }
-
       Model.prototype.maxAge = 4000;
-
       Model.prototype.data = [];
-
       Model.prototype.add = function(data) {
         data.age = (new Date).getTime();
         return this.data.push(data);
       };
-
       Model.prototype.update = function() {
-        var now,
-          _this = this;
+        var now;
         now = (new Date).getTime();
-        return this.data = _.filter(this.data, function(elem) {
-          return now - elem.age < _this.maxAge;
-        });
+        return this.data = _.filter(this.data, __bind(function(elem) {
+          return now - elem.age < this.maxAge;
+        }, this));
       };
-
       Model.prototype.toPath = function(chart) {
         var coord, dot, now, path, toCoord, _i, _len, _ref;
-        if (this.data.length === 0) return;
+        if (this.data.length === 0) {
+          return;
+        }
         now = (new Date).getTime();
         toCoord = function(data) {
           return {
@@ -65,23 +62,17 @@
         }
         return path;
       };
-
       return Model;
-
-    })(Backbone.Model);
-    Chart.Collection = (function(_super) {
-
-      __extends(Collection, _super);
-
+    })();
+    Chart.Collection = (function() {
+      __extends(Collection, Backbone.Collection);
       function Collection() {
         this.render = __bind(this.render, this);
         this.update = __bind(this.update, this);
         this.parse = __bind(this.parse, this);
         Collection.__super__.constructor.apply(this, arguments);
       }
-
       Collection.prototype.model = Chart.Collection;
-
       Collection.prototype.parse = function(data) {
         var chart, newChart, raw;
         raw = data;
@@ -104,7 +95,6 @@
           return chart.add(data);
         }
       };
-
       Collection.prototype.update = function() {
         var model, _i, _len, _ref, _results;
         _ref = this.models;
@@ -115,7 +105,6 @@
         }
         return _results;
       };
-
       Collection.prototype.render = function() {
         var model, _i, _len, _ref, _results;
         _ref = this.models;
@@ -126,25 +115,17 @@
         }
         return _results;
       };
-
       return Collection;
-
-    })(Backbone.Collection);
-    Oscilloscope = (function(_super) {
-
-      __extends(Oscilloscope, _super);
-
+    })();
+    Oscilloscope = (function() {
+      __extends(Oscilloscope, Backbone.View);
       function Oscilloscope() {
         this.render = __bind(this.render, this);
         Oscilloscope.__super__.constructor.apply(this, arguments);
       }
-
       Oscilloscope.prototype.el = $('#oscilloscope-container');
-
       Oscilloscope.prototype.width = 800;
-
       Oscilloscope.prototype.height = 150;
-
       Oscilloscope.prototype.render = function() {
         var path;
         path = this.model.toPath(this);
@@ -152,7 +133,6 @@
           path: path
         });
       };
-
       Oscilloscope.prototype.initialize = function(id, model) {
         var template;
         this.id = id;
@@ -168,33 +148,22 @@
           'stroke-width': 5
         });
       };
-
       return Oscilloscope;
-
-    })(Backbone.View);
-    Meter = (function(_super) {
-
-      __extends(Meter, _super);
-
+    })();
+    Meter = (function() {
+      __extends(Meter, Backbone.View);
       function Meter() {
         Meter.__super__.constructor.apply(this, arguments);
       }
-
       Meter.prototype.el = $('#meter-container');
-
       Meter.prototype.width = 800;
-
       Meter.prototype.height = 150;
-
       Meter.prototype.initialize = function() {
         return console.log("meter " + this.attributes.id + " initialized");
       };
-
       Meter.prototype.render = function() {};
-
       return Meter;
-
-    })(Backbone.View);
+    })();
     chart = new Chart.Collection;
     socket = io.connect();
     socket.on('data', function(data) {
@@ -223,5 +192,4 @@
       return chart.render();
     });
   });
-
 }).call(this);
